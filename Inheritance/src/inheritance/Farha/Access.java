826786e896def2ba4ae42a76b6497ed547b34915
FarhaIslam <farhaislam999@gamil.com>
2017-10-30 08:28:10 -0700

    creat a class C and class D that extends class C and also a member variable in class C is private

--- Inheritance/src/inheritance/Farha/Access.java
+++ Inheritance/src/inheritance/Farha/Access.java
@@ -1,5 +1,20 @@
 package inheritance.Farha;
-
+class C{
+	int a;
+	private b;
+	void setab(int x , int y){
+		x = a;
+		y = b;
+	}
+	
+}
+class D extends C{
+	int total;
+	void sum(){
+		total = a + b;
+	}
+	
+}
 public class Access {
 
 }
