2e5776e967af0fba9508f5b2e957b10aa57135d7
FarhaIslam <farhaislam999@gamil.com>
2017-11-02 10:40:15 -0700

    Creat a class Box2 that has private variable and has four constructor which has different parameter.

--- Inheritance/src/inheritance/Farha/DemoShipment1.java
+++ Inheritance/src/inheritance/Farha/DemoShipment1.java
@@ -1,5 +1,42 @@
 package inheritance.Farha;
 
+class Box2 {  
+	  private double width; 
+	  private double height;   
+	  private double depth; 
+	    
+
+	Box2(Box2 ob) {   
+	      width = ob.width;     
+	      height = ob.height;     
+	      depth = ob.depth;  
+	     
+	     }  
+  Box2(double w, double h, double d) {    
+	    width = w;     
+	    height = h;   
+	    depth = d;   
+	     
+      }  
+	    
+	Box2() {     
+	    width = -1;  
+	    height = -1;
+	    depth = -1;    
+	    
+	   }  
+	     
+	Box2(double len) {     
+	         
+		width = height = depth = len;  
+		
+	}  
+	   
+	double volume() {     
+		return width * height * depth;   
+	}
+	
+}
 public class DemoShipment1 {
 
 }
