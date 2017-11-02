92bd413b48a1168f15972ec47a35bc420b9c4942
FarhaIslam <farhaislam999@gamil.com>
2017-11-02 10:23:43 -0700

    creat a class Shipment that extendes the class BoxWeight1 . Shipment class has four constructor which has different parameter.

--- Inheritance/src/inheritance/Farha/DemoShipment.java
+++ Inheritance/src/inheritance/Farha/DemoShipment.java
@@ -64,6 +64,37 @@ class BoxWeight1 extends Box2 {
 	 } 
 	 
 } 
+
+
+class Shipment extends BoxWeight1 {   
+      double cost;  
+ 
+     Shipment(Shipment ob) { 
+        super(ob);     
+        cost  = ob.cost;  
+
+      }  
+
+     Shipment(double w, double h, double d,double m, double c) {     
+        super(w, h, d, m); 
+        cost = c;  
+
+     }  
+
+     Shipment() {     
+        super();     
+        cost = -1;  
+
+    }  
+
+    Shipment(double len, double m, double c) {   
+
+        super(len, m);    
+        cost = c;  
+   
+     }
+
+} 
 public class DemoShipment {
 
 }
