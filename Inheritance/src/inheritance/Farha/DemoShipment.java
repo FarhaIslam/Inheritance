d7e98e4e2296a0d8e6d3b4cca525f8e725503e7e
FarhaIslam <farhaislam999@gamil.com>
2017-11-02 10:15:37 -0700

    creat a class BoxWeight1 that extendes class Box2. BoxWeight class has four constructor that has different parameters.

--- Inheritance/src/inheritance/Farha/DemoShipment.java
+++ Inheritance/src/inheritance/Farha/DemoShipment.java
@@ -37,6 +37,33 @@ class Box2 {
 		
 	    } 
 	} 
+
+class BoxWeight1 extends Box2 {  
+	
+	double weight; 
+	BoxWeight1(BoxWeight1 ob) { 
+	     super(ob);     
+	     weight = ob.weight; 
+	  }  
+	 
+	BoxWeight1(double w, double h, double d, double m) {     
+	        super(w, h, d);    
+	        weight = m;  
+	        
+	 }  
+	   
+	 BoxWeight1() {    
+		 super();     
+		 weight = -1; 
+	 }
+	 
+	 BoxWeight1(double len, double m) {     
+		 super(len);    
+		 weight = m;  
+		 
+	 } 
+	 
+} 
 public class DemoShipment {
 
 }
