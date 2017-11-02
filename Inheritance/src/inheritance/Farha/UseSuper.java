5747a7888f980a2ccd64ed5fc4371d09eb06f331
FarhaIslam <farhaislam999@gamil.com>
2017-11-02 09:55:46 -0700

    creat a class E and F where F extendes E. Here two class has a variable that has same name .

--- Inheritance/src/inheritance/Farha/UseSuper.java
+++ Inheritance/src/inheritance/Farha/UseSuper.java
@@ -1,5 +1,22 @@
 package inheritance.Farha;
 
+class E {
+	int k;
+	} 
+class F extends E {   
+	int k;  
+  F(int m, int n) {    
+	super.k = m;
+   
+     }
+  
+	void show(){     
+		System.out.println("k in superclass: " + super.k);     
+		System.out.println("k in subclass: " + k);   
+	
+  }
+}
 public class UseSuper {
 
+	
 }
