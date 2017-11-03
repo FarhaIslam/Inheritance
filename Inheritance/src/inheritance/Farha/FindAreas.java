c02857d79d656f79beed314ac6fb0f8b3a1486be
FarhaIslam <farhaislam999@gamil.com>
2017-11-03 07:26:32 -0700

    creat a class Figure that has two double variable and has a constructor and also has a method.This class store the dimension of objects.

--- Inheritance/src/inheritance/Farha/FindAreas.java
+++ Inheritance/src/inheritance/Farha/FindAreas.java
@@ -1,5 +1,18 @@
 package inheritance.Farha;
-
+class Figure {   
+    double dim1;   
+    double dim2;  
+  Figure(double a, double b) {    
+         dim1 = a;                       
+         dim2 = b;   
+     }  
+  
+  double area() {     
+         
+	  System.out.println("Area for Figure is undefined.");    
+          return 0;  
+    } 
+}  
 public class FindAreas {
 
 }
