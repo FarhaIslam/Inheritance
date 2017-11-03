f1e7dc12af1d302542d945f8437630a31d78937b
FarhaIslam <farhaislam999@gamil.com>
2017-11-03 08:15:53 -0700

    creat a class A3 in abstract  and a method in this class is also declares in abstract.

--- Inheritance/src/inheritance/Farha/AbstractDemo.java
+++ Inheritance/src/inheritance/Farha/AbstractDemo.java
@@ -1,5 +1,15 @@
 package inheritance.Farha;
 
+abstract class A3 {   
+    
+	abstract void callme();  
+    
+    void callmetoo() {    
+        System.out.println("This is a concrete method.");  
+   }
+}  
+
+
 public class AbstractDemo {
 
 }
