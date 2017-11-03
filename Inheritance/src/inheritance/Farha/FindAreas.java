a7ed0f4f0f80d2008beb36e8381a63b70cee9d29
FarhaIslam <farhaislam999@gamil.com>
2017-11-03 07:30:55 -0700

    creat a class Rectangle that extends the class Figure .This class has a constructor and a method and computes the area of the object

--- Inheritance/src/inheritance/Farha/FindAreas.java
+++ Inheritance/src/inheritance/Farha/FindAreas.java
@@ -12,6 +12,20 @@ class Figure {
 	  System.out.println("Area for Figure is undefined.");    
           return 0;  
     } 
+} 
+
+class Rectangle extends Figure {   
+
+    Rectangle(double a, double b) {     
+         super(a, b);  
+
+ }  
+    double area() {    
+     
+	System.out.println("Inside Area for Rectangle.");    
+      return dim1 * dim2;  
+
+   }
 }  
 public class FindAreas {
 
