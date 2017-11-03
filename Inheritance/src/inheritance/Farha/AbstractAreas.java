cb6a78976a4b4c1f0c118be882ea6a6aa4672a3e
FarhaIslam <farhaislam999@gamil.com>
2017-11-03 08:38:08 -0700

    creat a abstract class Figure1 that has a constructor and has a abstract method.

--- Inheritance/src/inheritance/Farha/AbstractAreas.java
+++ Inheritance/src/inheritance/Farha/AbstractAreas.java
@@ -1,5 +1,18 @@
 package inheritance.Farha;
 
+abstract class Figure1 {   
+
+    double dim1;   
+    double dim2;  
+
+    Figure1(double a, double b) {    
+         dim1 = a;     
+         dim2 = b;  
+
+      }  
+    abstract double area(); 
+
+}  
 public class AbstractAreas {
 
 }
