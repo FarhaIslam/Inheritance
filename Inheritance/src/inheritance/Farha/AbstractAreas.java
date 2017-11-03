ba4d38ab210987e20a9f209104db7065390e55b0
FarhaIslam <farhaislam999@gamil.com>
2017-11-03 08:41:02 -0700

    creat a class Rectangle1 that extends the class Figure1 and this class has a method which is the same name in Figure1 class method.

--- Inheritance/src/inheritance/Farha/AbstractAreas.java
+++ Inheritance/src/inheritance/Farha/AbstractAreas.java
@@ -13,6 +13,19 @@ abstract class Figure1 {
     abstract double area(); 
 
 }  
+
+class Rectangle1 extends Figure1{   
+    Rectangle1(double a, double b) {     
+     super(a, b);  
+
+     }  
+    double area() {    
+    System.out.println("Inside Area for Rectangle.");    
+     return dim1 * dim2;  
+     
+    }
+
+}  
 public class AbstractAreas {
 
 }
