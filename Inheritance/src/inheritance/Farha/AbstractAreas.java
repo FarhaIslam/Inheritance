413515e7b87e3ada082a6f91fa51b74eabfb8902
FarhaIslam <farhaislam999@gamil.com>
2017-11-03 08:47:24 -0700

    creat a abstract class reference that can be used to refer to an object of any class derived from.

--- Inheritance/src/inheritance/Farha/AbstractAreas.java
+++ Inheritance/src/inheritance/Farha/AbstractAreas.java
@@ -41,5 +41,14 @@ class Triangle1 extends Figure1 {
 
 }  
 public class AbstractAreas {
+	 public static void main(String args[]) {   
+	      
+		 Rectangle1 r = new Rectangle1(9, 5);    
+         Triangle1 t = new Triangle1(10, 8);     
+         
+         Figure1 figref; 
+         figref = r;     
+           System.out.println("Area is " + figref.area());  
 
+	 }
 }
