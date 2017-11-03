7e48951fc5d579b03551b3e93008b51796961701
FarhaIslam <farhaislam999@gamil.com>
2017-11-03 07:21:18 -0700

    creat three object of three class and  a reference of class A2 which assigns a reference to each type of object and using this reference invoked the same name method of all class.

--- Inheritance/src/inheritance/Farha/Dispatch.java
+++ Inheritance/src/inheritance/Farha/Dispatch.java
@@ -5,17 +5,34 @@ class A2{
 	   void callme() {    
 	         System.out.println("Inside A2's callme method");   
 	     } 
-	}  
+  }  
 	class B2 extends A2 {   
 	    void callme() {     
 	         System.out.println("Inside B2's callme method"); 
 	    }
-	 }  
+  }  
 	class C2 extends A2 {   
 	    void callme() {    
 	         System.out.println("Inside C2's callme method"); 
-	    } 
-	}  
+        } 
+  }  
 public class Dispatch {
 
+
+    public static void main(String args[]) {    
+          A2 a = new A2();     
+          B2 b = new B2();    
+          C2 c = new C2();  
+
+          A2 r;  
+          r = a; 
+          r.callme();   
+          
+          r = b;   
+          r.callme(); 
+          
+         r = c;    
+         r.callme();   
+         
+    } 
 }
