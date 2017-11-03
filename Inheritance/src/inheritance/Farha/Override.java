e4e9fc2761fcf3fd762f3ae2c0df04b4cc0599b7
FarhaIslam <farhaislam999@gamil.com>
2017-11-02 23:44:38 -0700

    creat a class Override and creat a object of class B1 and call the constructor of class B1 and the B1 class method override the A1 class method.

--- Inheritance/src/inheritance/Farha/Override.java
+++ Inheritance/src/inheritance/Farha/Override.java
@@ -34,4 +34,9 @@ class B1 extends A1 {
 }  
 public class Override {
 
+	 public static void main(String args[]) {    
+         B1 subOb = new B1(1, 2, 3);  
+               subOb.show();  
+
+}
 }
