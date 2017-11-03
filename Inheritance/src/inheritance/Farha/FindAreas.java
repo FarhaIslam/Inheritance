7d9169b3f7ed02e736e12946f1de3838339c6768
FarhaIslam <farhaislam999@gamil.com>
2017-11-03 07:33:29 -0700

    creat a Triangle that also extends the class Figure and has a constructor and a method.

--- Inheritance/src/inheritance/Farha/FindAreas.java
+++ Inheritance/src/inheritance/Farha/FindAreas.java
@@ -26,7 +26,21 @@ class Rectangle extends Figure {
       return dim1 * dim2;  
 
    }
-}  
+} 
+
+class Triangle extends Figure {   
+
+    Triangle(double a, double b) {     
+           super(a, b);  
+
+     }  
+ double area() {    
+       
+     System.out.println("Inside Area for Triangle.");    
+       return dim1 * dim2 / 2;  
+
+    } 
+}
 public class FindAreas {
 
 }
