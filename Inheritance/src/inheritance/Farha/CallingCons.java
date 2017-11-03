7b629a27f460319a8070364be456954b607962da
FarhaIslam <farhaislam999@gamil.com>
2017-11-02 22:15:59 -0700

    creat three class M and N and O where all three classes has a constructor and class N extends class M and also class O extendes class N

--- Inheritance/src/inheritance/Farha/CallingCons.java
+++ Inheritance/src/inheritance/Farha/CallingCons.java
@@ -1,4 +1,34 @@
 package inheritance.Farha;
+ 
+class M{   
+    
+    M() {    
+     
+        System.out.println("Inside M's constructor.");
+
+      }
+
+}
+
+class N extends M{   
+    
+   N() {    
+
+       System.out.println("Inside N's constructor.");   
+
+    }
+
+}  
+
+class O extends N { 
+
+  O() {    
+
+       System.out.println("Inside O's constructor.");  
+
+     }
+
+}  
 
 public class CallingCons {
 
