4864d7cd7172e4d9833eaecfbba15fcda3234b72
FarhaIslam <farhaislam999@gamil.com>
2017-11-02 23:37:18 -0700

    creat A1 that has constructor  and has a method.

--- Inheritance/src/inheritance/Farha/Override.java
+++ Inheritance/src/inheritance/Farha/Override.java
@@ -1,5 +1,21 @@
 package inheritance.Farha;
 
+class A1 {  
+
+    int i, j;   
+    A1(int a, int b) {    
+         
+             i = a;     
+             j = b;  
+
+       }  
+
+    void show() {    
+
+              System.out.println("i and j: " + i + " " + j);   
+           } 
+    } 
+  
 public class Override {
 
 }
