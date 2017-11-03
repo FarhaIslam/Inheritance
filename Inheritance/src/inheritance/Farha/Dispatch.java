5eab2f0e6e0cb71aba7369bae910ea5381b1be08
FarhaIslam <farhaislam999@gamil.com>
2017-11-03 07:11:40 -0700

    creat three class A2,B2 and C2 where class B2 extends class A2 anh class C2 extends class B2 and three class has a same name method.

--- Inheritance/src/inheritance/Farha/Dispatch.java
+++ Inheritance/src/inheritance/Farha/Dispatch.java
@@ -1,5 +1,21 @@
 package inheritance.Farha;
 
+class A2{   
+
+	   void callme() {    
+	         System.out.println("Inside A2's callme method");   
+	     } 
+	}  
+	class B2 extends A2 {   
+	    void callme() {     
+	         System.out.println("Inside B2's callme method"); 
+	    }
+	 }  
+	class C2 extends A2 {   
+	    void callme() {    
+	         System.out.println("Inside C2's callme method"); 
+	    } 
+	}  
 public class Dispatch {
 
 }
